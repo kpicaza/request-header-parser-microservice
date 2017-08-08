f067d2335c36a624e4a6526aa35380eb266cccc0
kpicaza <kpicaza.sound@gmail.com>
2017-08-09 00:39:14 +0200

    create express app

---
+++ app/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var pug = require('pug');
+var service = require('./header-parser/service');
+
+var app = express();
+
+app.get("/", function (request, response) {
+  var view = pug.compileFile(path.resolve('views/index.pug'));
+
+  response.send(view({
+    example: JSON.stringify(service(request))
+  }));
+});
+
+app.get("/whoami", function (request, response) {
+  response.send(JSON.stringify(service(request)));
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
+
